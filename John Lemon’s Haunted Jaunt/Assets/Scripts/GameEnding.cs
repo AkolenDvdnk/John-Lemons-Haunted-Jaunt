e3e63f8ad1eb5232225fe83cfdad9660c05c0645
AkolenDvdnk <ak0lenfs@gmail.com>
2022-02-17 17:22:19 +0200

    Ending the Game

---
+++ John Lemon’s Haunted Jaunt/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    private float displayImageDuration = 3.5f;
+    private float fadeDuration = 1f;
+    private float timer;
+
+    private bool isPlayerAtExit = false;
+
+    private void Update()
+    {
+        if (!isPlayerAtExit)
+            return;
+
+        Victory();
+    }
+    private void Victory()
+    {
+        timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.alpha = timer / fadeDuration;
+
+        if (timer > fadeDuration + displayImageDuration)
+        {
+            Debug.Log("Quit");
+            Application.Quit();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerAtExit = true;
+        }
+    }
+}
