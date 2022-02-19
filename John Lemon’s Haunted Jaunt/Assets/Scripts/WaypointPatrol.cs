43429dafebf8287b8a11dcfde4fc7bde23963268
AkolenDvdnk <ak0lenfs@gmail.com>
2022-02-19 12:38:47 +0200

    Dynamic Observers

---
+++ John Lemon’s Haunted Jaunt/Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    private int currentWaypointIndex;
+
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    private void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+    private void Update()
+    {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);  
+        }
+    }
+}
